5917e5c4502a683a6ac91f2b42f1dfc8e415e9cd
spl <simon@raspberrypi.org>
2015-02-18 11:27:51 +0000

    Message sent to lxpanel when default device is changed.

--- xfce4-mixer/xfce-mixer-window.c
+++ xfce4-mixer/xfce-mixer-window.c
@@ -351,6 +351,8 @@ xfce_mixer_window_action_use_card (GtkAction       *action,
   xfce_mixer_window_update_contents (window);
   
   xfce_mixer_card_combo_update (XFCE_MIXER_CARD_COMBO (window->soundcard_combo), card);
+  
+  system ("lxpanelctl alsaconf");
 }
 
 static void
